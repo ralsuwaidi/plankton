f882b78d89a009db3dcfe973eca5c4b8f1e9bbf3
Rashed AlSuwaidi <rashed.alsuwaidi@ai.gov.ae>
2023-10-10 09:58:16 +0400

    change to backend connection

--- telebot.py
+++ telebot.py
@@ -85,7 +85,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message: str = update.message.text
 
     # Send the user message to the MOF website chatbot API
-    response = requests.post("http://mof.codershq.ae/ask", json={"question": message})
+    response = requests.post("http://backend:9091/ask", json={"question": message})
 
     # Get the response from the MOF website chatbot API
     answer = json.loads(response.text)
